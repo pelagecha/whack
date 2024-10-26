627284b9e282ab496e29827730685dc65e6e17ea
amency <nikitapelagecha@gmail.com>
2024-10-27 00:26:37 +0100

    configured correct https

--- frontend/components/HomePage.tsx
+++ frontend/components/HomePage.tsx
@@ -33,7 +33,7 @@ export default function HomePage() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch("http://localhost:3000/home");
+                const response = await fetch("http://127.0.0.1:5000/home");
                 if (!response.ok) {
                     throw new Error("Network response was not ok");
                 }
