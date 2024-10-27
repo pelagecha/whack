00bf209c2ceb691a703fe5f744d0ccebe6202e2a
amency <nikitapelagecha@gmail.com>
2024-10-27 09:52:30 +0000

    final commit from me

--- frontend/components/InfoTiles.tsx
+++ frontend/components/InfoTiles.tsx
@@ -36,7 +36,7 @@ const InfoTiles: React.FC<InfoTilesProps> = ({
             </div>
             <div className="bg-white rounded-xl shadow-lg p-6 text-center">
                 <h2 className="text-2xl font-bold text-gray-700">
-                    Spending Increase
+                    Spending Change
                 </h2>
                 <p className="text-3xl text-yellow-500 font-extrabold mt-4">
                     {spendingIncrease.toFixed(2)}%
