3c2a40c9605d8f9e6d883f399c77a4e737909f0f
Jakub Slapek <superjakub1029@gmail.com>
2024-10-27 02:35:25 +0000

    small fix

--- backend/gpt.py
+++ backend/gpt.py
@@ -19,7 +19,7 @@ def run_rag(query, connection, accountno):
         model="gpt-4o-mini",
         messages=[
             {"role": "system", "content": f"You are a knowledgeable and friendly financial assistant. Your role is to provide clear, accurate, and helpful financial guidance. You aim to help users understand financial concepts, answer questions on personal finance topics (like budgeting, saving, investing, and debt management), and offer tips for financial wellness. Always prioritize accuracy and avoid making any guarantees, giving specific investment advice, or suggesting risky financial actions. Be supportive, respectful, and encourage users to seek professional advice when needed. Do not exceed 750 characters in your response. Tell the user you will not answer questions unrelated to helping them understand their account if their question isn't. If the answer is related to their account, use the following information: {records}"},
-            {"role": "user", "content": f"{text}"}
+            {"role": "user", "content": f"{query}"}
         ],
     )
     return completion.choices[0].message.content
